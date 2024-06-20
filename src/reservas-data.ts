3a69e476a2f7870793bfad78a939bf810fd527ad
adriandcoding <theamr91@gmail.com>
2024-06-20 17:40:20 +0200

    añadiendo propiedad desayuno

---
+++ src/reservas-data.ts
@@ -0,0 +1,20 @@
+export const reservas: Reserva[] = [
+  {
+    tipoHabitacion: "standard",
+    desayuno: false,
+    pax: 1,
+    noches: 3,
+  },
+  {
+    tipoHabitacion: "standard",
+    desayuno: false,
+    pax: 1,
+    noches: 4,
+  },
+  {
+    tipoHabitacion: "suite",
+    desayuno: true,
+    pax: 2,
+    noches: 1,
+  },
+];
